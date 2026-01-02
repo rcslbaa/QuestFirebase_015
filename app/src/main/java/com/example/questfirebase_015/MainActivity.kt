883b8a4407f9aff1414d105027c8ceac143c704a
rcslbaa <ifasmarani3125@gmail.com>
2026-01-02 21:12:05 +0700

    memperbarui Main Activity

--- app/src/main/java/com/example/questfirebase_015/MainActivity.kt
+++ app/src/main/java/com/example/questfirebase_015/MainActivity.kt
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    FirebaseTheme {
+    QuestFirebase_015Theme {
         Greeting("Android")
     }
 }
