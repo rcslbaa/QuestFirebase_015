02d86b9438e64c5d506ccaf234a8e39e8da87a2d
rcslbaa <ifasmarani3125@gmail.com>
2026-01-02 18:27:51 +0700

    menambahkan Peta Navigasi

---
+++ app/src/main/java/com/example/questfirebase_015/view/controlNavigasi/kt/PetaNavigasi.kt
@@ -0,0 +1,53 @@
+package com.example.questfirebase_015.view.controlNavigasi.kt
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questfirebase_015.view.EntrySiswaScreen
+import com.example.questfirebase_015.view.HomeScreen
+import com.example.questfirebase_015.view.route.DestinasiDetail
+import com.example.questfirebase_015.view.route.DestinasiEntry
+import com.example.questfirebase_015.view.route.DestinasiHome
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/$it")
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
