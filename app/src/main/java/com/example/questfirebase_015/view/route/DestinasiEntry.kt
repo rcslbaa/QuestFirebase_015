e235ca750df8267607744b8109dd31d7ce139c70
rcslbaa <ifasmarani3125@gmail.com>
2026-01-02 18:30:14 +0700

    menambahkan Destinasi Entry

---
+++ app/src/main/java/com/example/questfirebase_015/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.questfirebase_015.view.route
+
+import com.example.questfirebase_015.R
+import com.example.questfirebase_015.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
