5e767b3e755097402aa9c48ef69ae3b7968e5fa3
rcslbaa <ifasmarani3125@gmail.com>
2026-01-09 21:12:47 +0700

    menambahkan edit view model

--- app/src/main/java/com/example/questfirebase_015/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_015/viewmodel/EditViewModel.kt
@@ -1,2 +1,52 @@
 package com.example.questfirebase_015.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_015.modeldata.DetailSiswa
+import com.example.questfirebase_015.modeldata.UIStateSiswa
+import com.example.questfirebase_015.modeldata.toDataSiswa
+import com.example.questfirebase_015.modeldata.toUiStateSiswa
+import com.example.questfirebase_015.repositori.RepositorySiswa
+import com.example.questfirebase_015.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long = savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+        ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, validasiInput(detailSiswa))
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank()&& alamat.isNotBlank()&& telpon.isNotBlank()
+        }
+    }
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            try {
+                repositorySiswa.editSatuSiswa(idSiswa,uiStateSiswa.detailSiswa.toDataSiswa())
+                println("Update Sukses: $idSiswa")
+            }catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
+        }
+    }
+
+}
